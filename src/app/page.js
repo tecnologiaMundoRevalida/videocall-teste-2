d06ed805f63c8a25962273a474340f5fb59b95bb
Elder Oliveira Ext <elder.oliveira.ext@infracommerce.com.br>
2023-08-28 11:10:39 -0300

    primeiro commit

--- src/app/page.js
+++ src/app/page.js
@@ -1,95 +1,86 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+"use client"
+
+import { useEffect, useRef } from 'react';
+import { Socket, io } from 'socket.io-client';
 
 export default function Home() {
+  const localVideoRef = useRef();
+  const remoteVideoRef = useRef();
+  const socketRef = useRef();
+
+  useEffect(() => {
+    // Initialize socket connection
+    socketRef.current = io('https://web-socket-mundorevalida.com:3000',{autoConnect: false,secure:false});
+
+    socketRef.current.auth = {user_id: '1234528'}
+    socketRef.current.connect();
+
+    socketRef.current.emit('joinRoom', {training: '20', id: '123452'});
+    // Access user's camera
+    navigator.mediaDevices.getUserMedia({ video: true, audio: true })
+      .then((stream) => {
+        localVideoRef.current.srcObject = stream;
+        
+        // Emit 'offer' event
+        const peerConnection = new RTCPeerConnection(
+          {
+            iceServers: [
+              {
+                urls: "stun:stun.l.google.com:19302"
+              }
+            ]
+          }
+        );
+        stream.getTracks().forEach((track) => {
+          peerConnection.addTrack(track, stream);
+        });
+
+        // peerConnection.createOffer()
+        //   .then((offer) => peerConnection.setLocalDescription(offer))
+        //   .then(() => {
+        //     socketRef.current.emit('offer', {offer:peerConnection.localDescription,room:'20'});
+        //   });
+
+        // Listen for 'offerReceived' event
+        socketRef.current.on('offerReceived', (offer) => {
+          peerConnection.setRemoteDescription(new RTCSessionDescription(offer));
+            peerConnection.createAnswer()
+            .then((answer) => peerConnection.setLocalDescription(answer))
+            .then(() => {
+              socketRef.current.emit('answer', {answer:peerConnection.localDescription,room:'20'});
+            });
+            // peerConnection.setLocalDescription(new RTCSessionDescription(peerConnection.localDescription));
+        });
+        
+        // Listen for 'answer' event
+        // socketRef.current.on('answer', (answer) => {
+        //   peerConnection.setRemoteDescription(new RTCSessionDescription(answer));
+        // });
+
+        // Listen for 'ice-candidate' event
+        socketRef.current.on('iceCandidateReceived', (candidate) => {
+          peerConnection.addIceCandidate(new RTCIceCandidate(candidate));
+        });
+
+        // Create answer and emit 'answer' event
+        // peerConnection.onicecandidate = (event) => {
+        //   if (event.candidate) {
+        //     socketRef.current.emit('ice-candidate', event.candidate);
+        //   }
+        // };
+
+        // peerConnection.ontrack = (event) => {
+        //   remoteVideoRef.current.srcObject = event.streams[0];
+        // };
+      })
+      .catch((error) => console.error('Error accessing camera:', error));
+  }, []);
+
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+    <div>
+      <h1>WebRTC Video Call</h1>
+      <video ref={localVideoRef} autoPlay muted />
+      <video ref={remoteVideoRef} autoPlay />
+    </div>
+  );
 }
