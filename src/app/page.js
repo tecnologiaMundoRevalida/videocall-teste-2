7df816c24a3c382d48b6c0c4991f6fd0177062e4
Elder Oliveira Ext <elder.oliveira.ext@infracommerce.com.br>
2023-08-28 11:37:05 -0300

    teste servidor stun video call

--- src/app/page.js
+++ src/app/page.js
@@ -25,8 +25,13 @@ export default function Home() {
         const peerConnection = new RTCPeerConnection(
           {
             iceServers: [
+              // {
+              //   urls: "stun:stun.l.google.com:19302"
+              // },
               {
-                urls: "stun:stun.l.google.com:19302"
+                urls: "turn:openrelay.metered.ca:80",
+                username: "openrelayproject",
+                credential: "openrelayproject"
               }
             ]
           }
